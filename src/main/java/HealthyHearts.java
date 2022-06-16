ceedcf86e6d12e575068f5213613dd597330f88b
Freweini <frena5@yahoo.com>
2022-06-16 17:33:10 +0100

    RockPaperScissors-FirstCommit

---
+++ src/main/java/HealthyHearts.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HealthyHearts {
+    public static void main(String[] args){
+        Scanner myScaner = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int age = myScaner.nextInt();
+        int maxHeartRate = 220 - age;
+        int targetHeartRate = 50- (maxHeartRate * 85/100);
+        System.out.println("Your maximum heart rate should be:" + " " + maxHeartRate);
+        System.out.println(" Your target HR Zone is:" + " "+ targetHeartRate);
+    }
+}
